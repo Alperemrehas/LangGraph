25fce91b196a22b531f500cb788bffbaeb4eee78
Alperemre <alperemrehas@gmil.com>
2024-09-27 00:31:26 +0300

    fix: third edge added and fixed

--- main.py
+++ main.py
@@ -1,261 +1,65 @@
-import re
-import os 
-import json
-
-from typing import TypedDict, Annotated, Sequence
-import operator
-from langchain_core.messages import BaseMessage
-from langgraph.graph import Graph, StateGraph, END
-from dotenv import load_dotenv
-from langchain_community.chat_models import ChatOpenAI
-from langchain_openai import AzureChatOpenAI
-from rag_pipeline import EmbeddingsHandler
-from pydantic import BaseModel, Field
-from langchain_core.output_parsers import PydanticOutputParser, StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-
-
-
-load_dotenv()
-# assign AgentState as an empty dict
-#AgentState = {}
-
-# messages key will be assigned as an empty array. We will append new messages as we pass along nodes. 
-#AgentState["messages"] = []
-class AgentState(TypedDict):
-    messages: Annotated[Sequence[BaseMessage], operator.add]
-
-from pydantic import BaseModel, Field, ValidationError
-
-class IntentionSelector(BaseModel):
-    user_intention: str = Field(
-        description="User Intention",
-        example="H1 Reactive Service",
-        title="Intention",
-        type="string"
-    )
-
-    @classmethod
-    def validate_intention(cls, user_intention: str):
-        valid_intentions = [
-            "H1 Reactive Service - Replacement Defective Parts",
-            "H2 Preventative Service - HVAC Maintenance - Fuel Filter",
-            "H3 Proactive Service - Elevator - Dispenser Troubleshooting, time to replace a nozzle, hose, breakaway or filter"
-        ]
-        print("-> Validating Intention")
-        print(f"Intention: {user_intention}")
-        print(f"Valid Intentions: {valid_intentions}")
-        if user_intention not in valid_intentions:
-            raise ValueError(f"Invalid intention: {user_intention}. Must be one of {valid_intentions}")
-        return user_intention
-
-# Example usage
-'''try:
-    intention = IntentionSelector(Intention="H1 Reactive Service")
-    print(intention)
-except ValidationError as e:
-    print(e)'''
-
-class LLMHandler:
-    def __init__(self, temperature=0, azure_deployment="gpt-40"):
-        self.api_key = os.getenv("AZURE_OPENAI_API_KEY")
-        self.azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-
-        if not self.api_key or not self.azure_endpoint:
-            raise ValueError("API Key and Azure Endpoint must be set in environment variables.")
-
-        self.llm = AzureChatOpenAI(
-            azure_endpoint=self.azure_endpoint,
-            azure_deployment=azure_deployment,
-            api_version="2024-02-01",
-            api_key=self.api_key,
-            temperature=temperature,
-        )
-
-    def analyze_user_intention(self, state: AgentState):
-        print('-> Calling Intent Classifier')
-        messages = state['messages']
-        question = messages[-1]
-
-        # Get and print format instructions
-        print("**********************************************")
-        format_instructions = parser.get_format_instructions()
-        #print(f"Format Instructions: {format_instructions}")
-        prompt_template = f"""Your task is to understand user intention based on the user query.
-        Provide a well-structured JSON output containing the user intention among:
-        - H1 Reactive Service - Replacement Defective Parts,
-        - H2 Preventative Service - HVAC Maintenance - Fuel Filter or Printer Paper Replacement,
-        - H3 Proactive Service - Elevator - Dispenser Troubleshooting, time to replace a nozzle, hose, breakaway or filter.
-        The user query is: {question}""" 
-        prompt = ChatPromptTemplate.from_template(
-            template = prompt_template,
-            input_variable = [question],
-        )
-        complete_query = prompt.format(question=question)
-        print(f"Complete Query: {complete_query}")
-        try:
-        # Create the chain
-            chain = prompt | self.llm | parser
-
-            # Invoke the chain
-            response = chain.invoke({"question": question, "format_instructions": format_instructions})
-
-            # Extracting content from the response
-            # If the response is an object like AIMessage, extract the 'content' attribute.
-            if isinstance(response, BaseMessage):
-                response_text = response.content
-            else:
-                response_text = str(response)  # Convert response to string if it’s not the expected format.
-
-            # Print the response for debugging
-            print(f"Response Text: {response_text}")
-            
-            #clean_response = response_text.strip().replace('```json', '').replace('```', '').strip()
-
-            #clean_response = clean_response.replace("'", '"')
-
-            #print(f"Clean Response Text: {clean_response}")
-
-            parsed_response = json.loads(response_text)
-
-            #IntentionSelector.validate_intention(parsed_response["user_intention"])
-            #return {"Intention": parsed_response["user_intention"]}  # Return the intention directly as specified
-            return {"messages": [parsed_response["user_intention"]]}
-
-
-            # Parse the response text using the parser
-            #parsed_response = parser.parse(response_text)
-
-            # Validate the parsed response
-            #IntentionSelector.validate_intention(parsed_response.Intention)
-
-            #return {"messages": [parsed_response.Intention]}
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            raise
-        '''
-        template = f"""Your task is to understand user intention based on the user query.
-        Provide a well-structured JSON output containing the user intention among:
-        - H1 Reactive Service - Replacement Defective Parts,
-        - H2 Preventative Service - HVAC Maintenance - Fuel Filter or Printer Paper Replacement,
-        - H3 Proactive Service - Elevator - Dispenser Troubleshooting, time to replace a nozzle, hose, breakaway or filter.
-        The user query is: {question} {format_instructions}""" 
-        prompt = ChatPromptTemplate(
-            template = template,
-            messages=messages,
-            input_variable = [question],
-            partial_query = {"format_instructions": parser.get_format_instructions()},
-            pattern=re.compile(r"\`\`\`\n\`\`\`") 
-            )
-        #response = self.llm.invoke(complete_query)
-        
-        # Generate the complete query
-        #complete_query = prompt.make_query()
-
-        # Print the complete query for debugging
-        print(f"Complete Query: {prompt}")
-
-        try:
-            # Create the chain
-            chain = prompt | self.llm | parser
-
-            # Invoke the chain
-            response = chain.invoke({"question": question, "format_instructions": format_instructions})
-            
-            # Print the response for debugging
-            print(f"Response: {response}")
-
-            # Validate the parsed response
-            IntentionSelector.validate_intention(response.Intention)
-            
-            return {"messages": [response.Intention]}
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            raise
-    '''
-
-    def format_user_intention(self, intention):
-        print('-> Formatting User Intention')
-        USER_INTENTION = intention.upper()
-        return f"User Intention is {USER_INTENTION}"
-    
-    def database_checker(self, intention):
-        print('-> SQL Query')
-        return f"SELECT * FROM TABLE WHERE INTENTION = {intention}"
-    
-    def router(state):
-        print('-> Router ->')
-        
-        messages = state["messages"]
-        last_message = messages[-1]
-        print("last message:",last_message)
-
-        if 'Reactive' in last_message:
-            return 'RAG Call'
-        elif 'Preventative' in last_message:
-            return 'LLM Call'
-        #elif 'Proactive' in last_message:
-        else:
-            return 'end'
-
-parser = PydanticOutputParser(pydantic_object = IntentionSelector)
+from langgraph.graph import StateGraph, END
+from src.rag_pipeline import EmbeddingsHandler
+from src.agent import AgentState, LLMHandler
 
 def main():
     handler = LLMHandler()
     embeddings_handler = EmbeddingsHandler()
 
-
-
     graph = StateGraph(AgentState)
 
     graph.add_node("agent", handler.analyze_user_intention)
-    graph.add_node("RAG", handler.format_user_intention)
     graph.add_node("LLM", handler.database_checker)
-    #graph.add_node("LLM", embeddings_handler.function_3)
+    graph.add_node("DOC", handler.document_checker)
+    graph.add_node("RAG", handler.format_user_intention)
     
+    
+
     graph.set_entry_point("agent")
-    graph.add_conditional_edges("agent",handler.router,{
-        "RAG Call": "RAG",
-        "LLM Call": "LLM",
-    })
+    graph.add_conditional_edges(
+        "agent",
+        handler.router,
+        {
+            "RAG Call": "RAG",
+            "LLM Call": "LLM",
+            "DOC Call": "DOC"
+        },
+    )
 
     graph.add_edge("RAG", END)
     graph.add_edge("LLM", END)
+    graph.add_edge("DOC", END)
+
 
     app = graph.compile()
 
-    #query = 'Hi, my fuel filter is not working. Can you help me with that?'
-    #print(f"Input query: {query}")
-    inputs = {"messages": ["Hi, my fuel filter is not working. Can you help me with that?"]}
+    # query = 'Hi, my fuel filter is not working. Can you help me with that?'
+    # print(f"Input query: {query}")
+    #Greeting Message to Customer
+    print("Welcome to our Website. I am your Customer Support Agent John. How can I help you today?")
+
+    user_query = input("Please enter your query: ")
+
+    inputs = {"messages": [user_query]}
+    # H1 Question
+    '''inputs = {
+        "messages": [
+            "Hi, one of my part is sseems to be defective. Can you help me with that?"
+        ]
+    }
+    # H2 Question
+    # inputs = {"messages": ["Hi, my fuel filter is not working. Can you help me with that?"]}
+    # H3 Question
+    # inputs = {"messages": ["Hi,I wonder that what is the time for replacment for my nozzle? Can you help me with that?"]}'''
+    # Debugging: Print the graph structure
+    print("Graph Nodes:", graph.nodes)
+    print("Graph Edges:", graph.edges)
+
+    # Debugging: Check the output of the router function
+    #router_output = handler.router(inputs)
+    #print(f"Router output: {router_output}")
     out = app.invoke(inputs)
-    
-    '''workflow = Graph()
-
-    workflow.add_node("analyze_intention", handler.analyze_user_intention)
-    workflow.add_node("format_intention", handler.format_user_intention)
-    workflow.add_node("embeddings", embeddings_handler.function_3)
-
-    workflow.add_edge('analyze_intention', 'format_intention')
-    workflow.add_edge('format_intention', 'embeddings')
-
-    workflow.set_entry_point("analyze_intention")
-    workflow.set_finish_point("embeddings")
-
-    app = workflow.compile()
-
-    try:
-        app.invoke(query)
-
-        for output in app.stream(query):
-            for key, value in output.items():
-                print(f"Output from node '{key}':")
-                print("----")
-                print(value)
-            print("\n----\n")
-
-    except Exception as e:
-        print(f"An error occurred: {e}")'''
-    
+    print(out)
 
 if __name__ == "__main__":
     main()
--- src/agent.py
+++ src/agent.py
@@ -1,25 +1,34 @@
+import os
+import json
+import ast
 from typing import TypedDict, Annotated, Sequence
 import operator
 from langchain_core.messages import BaseMessage
-from langgraph.graph import Graph, StateGraph, END
-import os 
-from dotenv import load_dotenv
-from langchain_community.chat_models import ChatOpenAI
 from langchain_openai import AzureChatOpenAI
-from rag_pipeline import EmbeddingsHandler
-from pydantic import BaseModel, Field
-from langchain_core.output_parsers import PydanticOutputParser, StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-
+from pydantic import BaseModel, Field, ValidationError
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 
 class AgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], operator.add]
 
 class IntentionSelector(BaseModel):
-    Intention: str = Field(description="User Intention", example="H1: Reactive Service - Replacement Defective Parts")
+    user_intention: str = Field(
+        description="user_intention",
+        example="H1",
+        title="Intention",
+        type="string"
+    )
+
+    @classmethod
+    def validate_intention(cls, user_intention: str):
+        valid_intentions = ["H1", "H2", "H3"]
+        if user_intention not in valid_intentions:
+            raise ValueError(f"Invalid intention: {user_intention}. Must be one of {valid_intentions}")
+        return user_intention
 
 class LLMHandler:
-    def __init__(self, temperature=0.7, azure_deployment="gpt-40"):
+    def __init__(self, temperature=0, azure_deployment="gpt-40"):
         self.api_key = os.getenv("AZURE_OPENAI_API_KEY")
         self.azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
 
@@ -35,40 +44,69 @@ class LLMHandler:
         )
 
     def analyze_user_intention(self, state: AgentState):
-        print('-> Calling Intent Classifier')
         messages = state['messages']
         question = messages[-1]
 
-        template = """Your task is to understand user intention based on the user query.
-        Only output the intention among ['H1: Reactive Service - Replacement Defective Parts',
-        'H2: Proactive Service - Maintenance', 'H3: Inquiry - General Information']."""
-        # Add more logic here as needed
+        prompt_template = """Your task is to understand user intention based on the user query.
+        Provide a well-structured JSON output containing the user intention among:
+        [H1, H2, H3]. Don't include reasoning. H1 refers Reactive Service - Replacement Defective Parts,
+        H2 refers Preventative Service, HVAC Maintenance,Fuel Filter or Printer Paper Replacement,
+        H3 refers Proactive Service, Elevator, Dispenser Troubleshooting,time to replace a nozzle, hose, breakaway or filter.
+        Following is the user query. \n{format_instructions}\n{question}"""
 
-class Agent:
-    def __init__(self, graph):
-        self.app = graph.compile()
+        prompt = PromptTemplate.from_template(
+            template=prompt_template,
+            input_variable=[question],
+            partial_variables={"format_instructions": parser.get_format_instructions()},
+        )
+        complete_query = prompt.format(question=question)
 
-    def invoke(self, inputs):
-        return self.app.invoke(inputs)
+        try:
+            chain = prompt | self.llm | parser
+            response = chain.invoke({"question": question, "format_instructions": parser.get_format_instructions()})
 
-    def run_workflow(self, query):
-        graph = Graph()
+            if isinstance(response, BaseMessage):
+                response_text = response.content
+            else:
+                response_text = str(response)
 
-        graph.add_node("agent", LLMHandler.analyze_user_intention)
-        graph.add_node("RAG", LLMHandler.format_user_intention)
-        graph.add_node("LLM", EmbeddingsHandler.function_3)
-        
-        graph.set_entry_point("agent")
-        graph.add_conditional_edges("agent", LLMHandler.router, {
-            "RAG Call": "RAG",
-            "LLM Call": "LLM",
-        })
+            return {"messages": [response.user_intention]}
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            raise
 
-        graph.add_edge("RAG", END)
-        graph.add_edge("LLM", END)
+    def format_user_intention(self, intention):
+        print(f"User intention: {intention} Calling --> RAG Call")
+        complete_query = "Answer this question like you are checking a part order. \n{question}"
+        response = self.llm.invoke(complete_query)
+        return {"messages": [response.content]}
 
-        app = graph.compile()
+    def database_checker(self, intention):
+        print(f"User intention: {intention} Calling --> LLM Call")
+        complete_query = "Answer this question like you are checking a database for a customer need. \n{question}"
+        response = self.llm.invoke(complete_query)
+        return {"messages": [response.content]}
+    
+    def document_checker(self, intention):
+        print(f"User intention: {intention} Calling --> Document Call")
+        complete_query = "Answer this question like you are checking a document for a customer need. \n{question}"
+        response = self.llm.invoke(complete_query)
+        return {"messages": [response.content]}
 
-        inputs = {"messages": [query]}
-        out = app.invoke(inputs)
-        return out
+    def router(self, state):
+        messages = state["messages"]
+        last_message = messages[-1]
+
+        if 'H1' in last_message:
+            print('H1')
+            return 'RAG Call'
+        elif 'H2' in last_message:
+            print('H2')
+            return 'LLM Call'
+        elif 'H3' in last_message:
+            print('H3')
+            return 'DOC Call'
+        else:
+            return 'end'
+
+parser = PydanticOutputParser(pydantic_object = IntentionSelector)
